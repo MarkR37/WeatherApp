ed7462b8fde8f4f4c1d0517a2fe3a3cf034e630f
Mark <mdroth7@gmail.com>
2022-03-06 21:05:05 -0500

    Made a function If user wants to check another zipcode, allow them, else exit. fixed displaying the country and city.

--- src/APIResult.java
+++ src/APIResult.java
@@ -1,9 +1,15 @@
 import java.util.List;
 
+
 public class APIResult {
     public Coord coord;
     public List<Weather> weather;
+    public Main main;
+    public Wind wind;
+    public Sys sys;
     public String base;
+
+    public String name; // its just an object within the json, no need for a class.
 
     public static class Coord {
         public double lon;
@@ -11,6 +17,21 @@ public class APIResult {
     }
 
     public static class Weather {
-
+        public String description;
     }
+
+    public static class Main {
+        public double temp;
+    }
+
+    public static class Wind {
+        public double speed;
+        public double deg;
+        public double gust;
+    }
+
+    public static class Sys {
+        public String country;
+    }
+
 }
--- src/WeatherApp.java
+++ src/WeatherApp.java
@@ -37,10 +37,41 @@ public class WeatherApp {
         w.run();
     }
 
-    public void run() throws IOException {
+    public void askUserIfNewZip() throws IOException {
+        System.out.println("If you want to check another zipcode Type 'y' . If you want to exit Type 'n'");
+        String x = scanner.next();
+        if (x.equals("y")) {
+            run();
+        }
+        System.exit(0);
+    }
+
+    public void displayInfo() throws IOException {
         String zipcode = promptForInput(ZIP_PROMPT);
         String weatherInformation = getWeatherInformation(zipcode);
         APIResult apiResult = gson.fromJson(weatherInformation, APIResult.class);
-        System.out.println(apiResult.coord.lat);
+        System.out.println("Current Coordinate Latitude: " + apiResult.coord.lat);
+        System.out.println("Current Coordinate Longitude: " + apiResult.coord.lon);
+        System.out.println("Temperature: " + apiResult.main.temp);
+        System.out.println("Wind-Angle: " + apiResult.wind.deg);
+        System.out.println("Wind-Speed: " + apiResult.wind.speed);
+        System.out.println("Wind-Gust: " + apiResult.wind.gust);
+        //System.out.println("Weather: " + apiResult.weather); // ask for some help since i'm unsure if this file is corrupted.
+        System.out.println("Country: " + apiResult.sys.country);
+        System.out.println("City: " + apiResult.name);
+        System.out.println("Zipcode: " + zipcode);
     }
+
+    public void run() throws IOException {
+        displayInfo();
+        askUserIfNewZip();
+    }
+
+
+    //copy to see json with proper units>
+    //https://api.openweathermap.org/data/2.5/weather?zip=33496,us&appid=69b953300c17615eb1dbd933559cadb2&units=imperial
+
+    //Goals: If user wants to check another zipcode, allow them, else exit. DONE
+    //Fix Weather to properly display information, currently it displays a token.
+
 }
